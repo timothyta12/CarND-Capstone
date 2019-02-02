7e3083edb2218f98afdb1cb20329479c44a7a61a
Timothy Ta <timothyta12@gmail.com>
2019-02-02 22:12:52 +0000

    fix: Adjusted PID controller
    Tuned the PID controller to have better lane tracking performance

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,8 +12,8 @@ class Controller(object):
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
-        kp = 0.2
-        ki = 0.1
+        kp = 0.5	
+        ki = 0.0
         kd = 0.0
         mn = 0.0 # Min throttle value
         mx = 0.2 # Max throttle value
